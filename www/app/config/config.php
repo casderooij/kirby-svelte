f95f6f489f5e01c9c347d4bd93e0c27685751327
casderooij <hello@casderooij.nl>
2021-11-06 11:44:29 +0100

    Initial commit

---
+++ www/app/config/config.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+  # core values
+  'arnoson.kirby-vite.rootDir' => '/../src/',
+  'arnoson.kirby-vite.outDir' => '/../public/assets/',
+
+  # optional values
+  'debug' => true,
+  'smartypants' => true
+];
